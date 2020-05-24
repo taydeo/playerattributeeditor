99abaea3d7ebc1899de0ae838361f058bd078033
Tadeo Diaz <td12233a@gmail.com>
2020-05-23 22:02:15 -0500

    Updated ResistModifier to be more reasonable.

--- src/main/java/net/cascene/playerattributeeditor/modifiers/ResistModifier.java
+++ src/main/java/net/cascene/playerattributeeditor/modifiers/ResistModifier.java
@@ -17,7 +17,13 @@ public class ResistModifier {
                 // looks for "playeratteditor.resist." and stores it in resistPermission
                 int permissionInteger = Integer.parseInt(valueOf(resistPermission.lastIndexOf(".") + 1));
                 // parses the permission to find the integer at the end of the permission node
-                double d = (double) permissionInteger * 0.; // this is one of the few that require a special formula
+                double d = (double) permissionInteger * 0.01; // this is one of the few that require a special formula, since max is 1.0
+                /*
+                * So lets say you have 4 as your integer, it makes the 4 into 0.04. Then that becomes your Knockback Resist Modifier.
+                * For any noticable effect, I would recommend using 10s. Multiplying by 0.01 lets you be more precise
+                * and calculated for your specific Knockback Resist values.
+                */
+                
                 Objects.requireNonNull(whoWasHit.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE)).setBaseValue(d);
                 // sets the base value, dont set it to 1 or else your player will be slower than usual!
 
